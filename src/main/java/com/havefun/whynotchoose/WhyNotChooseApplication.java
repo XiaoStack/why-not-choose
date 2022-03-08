6f364a41a5bacf64f2c819a6f4619759be57f556
yinyong <yinyong@powersi.com>
2022-03-08 14:53:11 +0800

    master初始化

---
+++ src/main/java/com/havefun/whynotchoose/WhyNotChooseApplication.java
@@ -0,0 +1,13 @@
+package com.havefun.whynotchoose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhyNotChooseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhyNotChooseApplication.class, args);
+    }
+
+}
